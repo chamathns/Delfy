e3255273b95c50250c7b8fc026151b9d39ab3a78
chamatx <chamath360@gmail.com>
2018-02-22 12:39:37 +0530

    import apache commons for File handling

--- src/main/java/sample/controller/LogInController.java
+++ src/main/java/sample/controller/LogInController.java
@@ -1,4 +1,33 @@
 package sample.controller;
 
-public class LogInController {
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LogInController implements Initializable{
+    @FXML
+    JFXButton buttonSignInPane, buttonRegisterPane;
+    @FXML
+    AnchorPane paneSignIn, paneRegister;
+    @FXML
+    public void handlePane( MouseEvent event){
+        if (event.getSource()==buttonRegisterPane){
+            paneRegister.toFront();
+        }else if (event.getSource()==buttonSignInPane){
+            paneSignIn.toFront();
+        }
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
