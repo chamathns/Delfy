eeccf4a08d00f414978fe81000ca616825c07d40
chamatx <chamath360@gmail.com>
2018-03-13 21:59:11 +0530

    implement file picker test class

---
+++ src/test/java/sample/controller/MainWindowControllerFilePickerTest.java
@@ -0,0 +1,12 @@
+package sample.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainWindowControllerFilePickerTest {
+
+    @Test
+    public void filePicker() {
+    }
+}
